ec2d647050a6dd3a3729d10da5faf6aae04f6637
'Clemy507' <'cpierre507@gmail.com'>
2017-09-12 12:16:41 +0200

    ajout classe voiture

---
+++ appligitmaven2/src/main/java/com/huios/monappligitmaven/appligitmaven2/metier/Voiture.java
@@ -0,0 +1,40 @@
+package com.huios.monappligitmaven.appligitmaven2.metier;
+
+public class Voiture {
+	private int numVoiture;
+	private String marque;
+	private String modele;
+	private Personne proprietaire;
+	public int getNumVoiture() {
+		return numVoiture;
+	}
+	public void setNumVoiture(int numVoiture) {
+		this.numVoiture = numVoiture;
+	}
+	public String getMarque() {
+		return marque;
+	}
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
+	public String getModele() {
+		return modele;
+	}
+	public void setModele(String modele) {
+		this.modele = modele;
+	}
+	public Personne getProprietaire() {
+		return proprietaire;
+	}
+	public void setProprietaire(Personne proprietaire) {
+		this.proprietaire = proprietaire;
+	}
+	@Override
+	public String toString() {
+		return "Voiture [numVoiture=" + numVoiture + ", marque=" + marque + ", modele=" + modele + "]";
+	}
+	
+	
+	
+
+}
